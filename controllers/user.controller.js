3eecd565e85f4a5a56b5cc775ea3a112a195d5d4
svasquez <santivasquez1@gmail.com>
2021-07-20 12:08:40 -0500

    Actualizacion de usuario y uso de midleware personalizado

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user.model');
-const { response, request } = require('express')
-const { validationResult } = require('express-validator')
+const { response, request } = require('express');
+const bcrypt = require('bcryptjs');
+
 const getUsuarios = async(req, res) => {
 
     const users = await User.find({}, 'name email role google');
@@ -13,16 +14,7 @@ const getUsuarios = async(req, res) => {
 
 const createUser = async(req = request, res = response) => {
 
-    const { email, password, name } = req.body;
-
-    const errores = validationResult(req);
-    if (!errores.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            msg: "Errores al enviar la información",
-            errors: errores.mapped()
-        })
-    }
+    const { email, password } = req.body;
 
     try {
         const existeEmail = await User.findOne({ email })
@@ -35,6 +27,11 @@ const createUser = async(req = request, res = response) => {
         }
 
         const user = new User(req.body);
+
+        //Encriptacin de contraseña
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+
         await user.save();
 
         res.json({
@@ -47,12 +44,59 @@ const createUser = async(req = request, res = response) => {
         res.status(500).json({
             ok: false,
             msg: "Error inesperado, revisar logs"
-        })
+        });
     }
 
 }
 
+const actualizarUsurario = async(req = request, res = response) => {
+
+    //TODO: Validar token y comprobar si es el usuario correcto
+
+    const uid = req.params.id;
+    try {
+
+        const userDb = await User.findById(uid);
+
+        if (!userDb) {
+            return res.status(404).send({
+                ok: false,
+                msg: 'No existe un usuario por ese id'
+            });
+        }
+
+        const { password, google, email, ...campos } = req.body;
+
+        if (userDb.email !== email) {
+            const existeEmail = await User.findOne({ email });
+            if (existeEmail) {
+                return res.status(400).send({
+                    ok: false,
+                    msg: "Ya existe un usuario con ese email"
+                });
+            }
+        }
+
+        campos.email = email
+        const userUpdate = await User.findByIdAndUpdate(uid, campos, { new: true });
+
+        res.status(200).send({
+            ok: true,
+            usurio: userUpdate
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Error inesperado, revisar logs"
+        });
+    }
+}
+
 module.exports = {
     getUsuarios,
-    createUser
+    createUser,
+    actualizarUsurario
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,17 +2,26 @@
     Ruta: /api/users
 */
 
-const { Router } = require('express')
-const { getUsuarios, createUser } = require('../controllers/user.controller')
-const { check } = require('express-validator')
+const { Router } = require('express');
+const { getUsuarios, createUser, actualizarUsurario } = require('../controllers/user.controller');
+const { check } = require('express-validator');
 const router = Router();
+const { validarCampos } = require('../middlewares/validarCampos')
 
 router.get('/', getUsuarios);
 router.post('/', [
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('password', 'El password es obligatorio').not().isEmpty(),
-        check('email', 'El email no tiene el formato correcto').isEmail()
+        check('email', 'El email no tiene el formato correcto').isEmail(),
+        validarCampos
     ],
     createUser);
+
+router.put('/:id', [
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email no tiene el formato correcto').isEmail(),
+    check('role', 'El rol es obligatorio').not().isEmpty(),
+    validarCampos
+], actualizarUsurario);
 
 module.exports = router;
